fe405f6dfad011822cdb9cc10c96df0f3e19aa23
CreakyCoss <1085892854@qq.com>
2018-03-07 20:28:53 +0800

    添加moment

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+moment = Moment()
 
 
 def create_app(config_name):
@@ -13,6 +15,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
     db.init_app(app)
     bootstrap.init_app(app)
+    moment.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
